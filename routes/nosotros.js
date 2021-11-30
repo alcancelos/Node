416c746dd20897897ac42e8a91bf4c0392e81b50
Alejandro Martín Cancelos <alcancelos@gmail.com>
2021-11-30 09:09:39 -0300

    first commit

---
+++ routes/nosotros.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var alex = 'Alejandro Martín Cancelos'
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('nosotros', { title: 'OP Jurídico Contable' });
+});
+
+module.exports = router;
